476cf0995571a79a135aeca312b63ce91802b7f9
Madeline Ellingson <93016306+Madeline-Ellingson@users.noreply.github.com>
2025-01-29 23:10:59 -0600

    #4 Create Beaver Mod Texture (#15)
    * #4 Re-added the minecraft version to the parchment mapping or it wouldn't work
    
    * #4 Add the beaver model, texture, and basic entity
    
    Signed-off-by: Madeline Ellingson <93016306+Madeline-Ellingson@users.noreply.github.com>
    
    * Removed a majority of the sample things in BeaverMod
    
    Signed-off-by: Madeline Ellingson <93016306+Madeline-Ellingson@users.noreply.github.com>
    
    * #4 added a comment for future development
    
    Signed-off-by: Madeline Ellingson <93016306+Madeline-Ellingson@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Madeline Ellingson <93016306+Madeline-Ellingson@users.noreply.github.com>

---
+++ src/main/java/com/madellz/beavermod/client/model/animal/BeaverModel.java
@@ -0,0 +1,158 @@
+package com.madellz.beavermod.client.model.animal;
+
+
+import com.madellz.beavermod.common.entity.animal.Beaver;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import net.minecraft.client.model.HierarchicalModel;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+
+import javax.annotation.Nonnull;
+
+/**
+ * BeaverModel represents the 3D model of the beaver entity in Minecraft.
+ * It defines the structure, parts, and animations of the beaver.
+ */
+public class BeaverModel extends HierarchicalModel<Beaver>
+{
+    private final ModelPart beaver;
+    private final ModelPart body;
+    private final ModelPart head;
+    private final ModelPart front_left_leg;
+    private final ModelPart front_right_leg;
+    private final ModelPart back_right_leg;
+    private final ModelPart back_left_leg;
+
+    /**
+     * Constructor that initializes model parts from the provided root model.
+     *
+     * @param root The root ModelPart containing all subparts.
+     */
+    public BeaverModel(ModelPart root)
+    {
+        this.beaver = root.getChild("beaver");
+        this.body = this.beaver.getChild("body");
+        this.head = this.body.getChild("head");
+        this.front_left_leg = this.body.getChild("front_left_leg");
+        this.front_right_leg = this.body.getChild("front_right_leg");
+        this.back_right_leg = this.body.getChild("back_right_leg");
+        this.back_left_leg = this.body.getChild("back_left_leg");
+    }
+
+    /**
+     * Creates and defines the structure of the beaver model.
+     *
+     * @return LayerDefinition containing the model structure.
+     */
+    public static LayerDefinition createBodyLayer()
+    {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+
+        PartDefinition beaver = partdefinition.addOrReplaceChild("beaver", CubeListBuilder.create(),
+                PartPose.offset(0.0F, 24.0F, 0.0F));
+
+        PartDefinition body = beaver.addOrReplaceChild("body",
+                CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -4.0F, -8.0F, 6.0F, 4.0F, 8.0F,
+                        new CubeDeformation(0.0F)).texOffs(0, 19).addBox(-3.0F, -5.0F, -4.0F, 6.0F, 1.0F, 4.0F,
+                        new CubeDeformation(0.0F)).texOffs(0, 24).addBox(-2.0F, -4.5F, -6.0F, 4.0F, 1.0F, 2.0F,
+                        new CubeDeformation(0.0F)).texOffs(0, 27).addBox(-1.0F, -2.0F, 0.0F, 2.0F, 2.0F, 1.0F,
+                        new CubeDeformation(0.0F)), PartPose.offset(0.0F, -2.0F, 2.0F));
+
+        PartDefinition tail_r1 = body.addOrReplaceChild("tail_r1",
+                CubeListBuilder.create().texOffs(0, 12).addBox(-2.0F, -1.0F, 0.0F, 4.0F, 1.0F, 6.0F,
+                        new CubeDeformation(0.0F)), PartPose.offsetAndRotation(0.0F, 0.0F, 0.0F, -0.2618F, 0.0F, 0.0F));
+
+        PartDefinition head = body.addOrReplaceChild("head",
+                CubeListBuilder.create().texOffs(20, 12).addBox(-2.0F, -3.75F, -0.5F, 4.0F, 3.0F, 2.0F,
+                        new CubeDeformation(0.0F)).texOffs(13, 29).addBox(-1.5F, -3.25F, -0.5F, 1.0F, 1.0F, 0.0F,
+                        new CubeDeformation(0.001F)).texOffs(29, 10).addBox(0.5F, -3.25F, -0.5F, 1.0F, 1.0F, 0.0F,
+                        new CubeDeformation(0.001F)).texOffs(28, 7).addBox(-1.0F, -2.0F, -1.0F, 2.0F, 1.0F, 1.0F,
+                        new CubeDeformation(0.0F)).texOffs(28, 11).addBox(-1.0F, -1.25F, -0.5F, 2.0F, 1.0F, 0.0F,
+                        new CubeDeformation(0.001F)), PartPose.offset(0.0F, -0.25F, -9.5F));
+
+        PartDefinition right_ear_r1 = head.addOrReplaceChild("right_ear_r1",
+                CubeListBuilder.create().texOffs(16, 28).addBox(-1.0F, -1.0F, -1.0F, 1.0F, 1.0F, 0.0F,
+                        new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(-1.0F, -3.35F, 1.6F, 0.0F, 0.6109F, 0.0F));
+
+        PartDefinition left_ear_r1 = head.addOrReplaceChild("left_ear_r1",
+                CubeListBuilder.create().texOffs(18, 28).addBox(-0.9F, -1.0F, -0.3F, 1.0F, 1.0F, 0.0F,
+                        new CubeDeformation(0.0F)),
+                PartPose.offsetAndRotation(2.1F, -3.35F, 1.5F, 0.0F, -0.6109F, 0.0F));
+
+        PartDefinition front_left_leg = body.addOrReplaceChild("front_left_leg",
+                CubeListBuilder.create().texOffs(6, 27).addBox(-1.0F, -2.0F, 0.0F, 2.0F, 2.0F, 1.0F,
+                        new CubeDeformation(0.0F)).texOffs(28, 3).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 1.0F, 1.0F,
+                        new CubeDeformation(0.0F)), PartPose.offset(2.0F, 2.0F, -7.0F));
+
+        PartDefinition front_right_leg = body.addOrReplaceChild("front_right_leg",
+                CubeListBuilder.create().texOffs(28, 0).addBox(-1.0F, -2.0F, 0.0F, 2.0F, 2.0F, 1.0F,
+                        new CubeDeformation(0.0F)).texOffs(28, 5).addBox(-1.0F, -1.0F, -1.0F, 2.0F, 1.0F, 1.0F,
+                        new CubeDeformation(0.0F)), PartPose.offset(-2.0F, 2.0F, -7.0F));
+
+        PartDefinition back_right_leg = body.addOrReplaceChild("back_right_leg",
+                CubeListBuilder.create().texOffs(12, 24).addBox(0.0F, -2.0F, 0.0F, 2.0F, 2.0F, 2.0F,
+                        new CubeDeformation(0.0F)).texOffs(20, 21).addBox(-1.0F, -1.0F, -2.0F, 3.0F, 1.0F, 3.0F,
+                        new CubeDeformation(0.0F)), PartPose.offset(-3.0F, 2.0F, -2.0F));
+
+        PartDefinition back_left_leg = body.addOrReplaceChild("back_left_leg",
+                CubeListBuilder.create().texOffs(20, 25).addBox(-1.0F, -2.0F, 0.0F, 2.0F, 2.0F, 2.0F,
+                        new CubeDeformation(0.0F)).texOffs(20, 17).addBox(-1.0F, -1.0F, -2.0F, 3.0F, 1.0F, 3.0F,
+                        new CubeDeformation(0.0F)), PartPose.offset(2.0F, 2.0F, -2.0F));
+
+        return LayerDefinition.create(meshdefinition, 48, 48);
+    }
+
+    /**
+     * Sets up animations for the beaver entity, defining how it moves.
+     *
+     * @param pEntity          The beaver entity instance.
+     * @param pLimbSwing       The swing progress of the entity's limbs.
+     * @param pLimbSwingAmount The intensity of the limb swing.
+     * @param pAgeInTicks      The entity's age in ticks, used for time-based animations.
+     * @param pNetHeadYaw      The horizontal rotation of the entity's head.
+     * @param pHeadPitch       The vertical rotation of the entity's head.
+     *
+     * TODO: #10 Implement Beaver Animations
+     */
+    @Override
+    public void setupAnim(@Nonnull Beaver pEntity, float pLimbSwing, float pLimbSwingAmount, float pAgeInTicks,
+            float pNetHeadYaw, float pHeadPitch)
+    {
+
+    }
+
+    /**
+     * Renders the beaver model to the screen using the provided rendering parameters.
+     *
+     * @param poseStack      The transformation stack for positioning and scaling.
+     * @param vertexConsumer The vertex consumer for rendering the model.
+     * @param packedLight    The lighting value for rendering.
+     * @param packedOverlay  The overlay texture for rendering.
+     * @param red            The red color multiplier.
+     * @param green          The green color multiplier.
+     * @param blue           The blue color multiplier.
+     * @param alpha          The transparency level.
+     */
+    @Override
+    public void renderToBuffer(@Nonnull PoseStack poseStack, @Nonnull VertexConsumer vertexConsumer, int packedLight,
+            int packedOverlay, float red, float green, float blue, float alpha)
+    {
+        beaver.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+    }
+
+    /**
+     * Retrieves the root model part of the beaver model.
+     *
+     * @return The root {@link ModelPart} representing the entire beaver model.
+     */
+    @Override
+    @Nonnull
+    public ModelPart root()
+    {
+        return beaver;
+    }
+}
---
+++ src/main/java/com/madellz/beavermod/common/entity/animal/Beaver.java
@@ -0,0 +1,59 @@
+package com.madellz.beavermod.common.entity.animal;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.Nonnull;
+
+/**
+ * The Beaver class represents a beaver entity in the game.
+ * It extends the {@link Animal} class and implements behavior specific to the beaver mob.
+ */
+public class Beaver extends Animal
+{
+    /**
+     * Creates a new Beaver entity with the given entity type and level.
+     *
+     * @param pEntityType The type of the entity (e.g., the specific entity type for Beaver).
+     * @param pLevel      The level (world) where the beaver will exist.
+     */
+    public Beaver(EntityType<? extends Beaver> pEntityType, Level pLevel)
+    {
+        super(pEntityType, pLevel);
+    }
+
+    /**
+     * Determines whether a specific item stack is considered food for the beaver.
+     *
+     * @param pStack The item stack being checked.
+     * @return False since beavers do not eat any item by default.
+     * <p>
+     * TODO: Implement food behavior for the beaver (e.g., specify items that beavers will eat). Add ticekt once created
+     */
+    @Override
+    public boolean isFood(@Nonnull ItemStack pStack)
+    {
+        return false;
+    }
+
+    /**
+     * Gets the offspring of this beaver and the other given parent.
+     * In this case, beavers do not currently breed, so this method returns null.
+     *
+     * @param pLevel       The level (world) where the offspring would be created.
+     * @param pOtherParent The other parent for the breeding process.
+     * @return Null, as beavers do not currently have offspring in this implementation.
+     * <p>
+     * TODO: Implement breeding. Add ticket once created.
+     */
+    @Override
+    public @Nullable AgeableMob getBreedOffspring(@Nonnull ServerLevel pLevel, @Nonnull AgeableMob pOtherParent)
+    {
+        return null;
+    }
+}
